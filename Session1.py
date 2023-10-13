759da5d5d77ea2800e043f7235ff7a6b3f2624da
mohmedbadri <56170819+mohamedbadri@users.noreply.github.com>
2023-10-12 18:26:41 -0700

    Add files via upload

---
+++ Session1.py
@@ -0,0 +1,16 @@
+#this writes Hello World!
+
+
+#this function writes Hello World!
+def PrintHelloWorld():
+    print("Hello, my name is Mohamed Badri. I don't really have a favourite movie but I really like the Fast and Furious Series.")
+
+
+#We call the function 
+def main():
+    PrintHelloWorld()
+
+
+#when we run the program this executes main()
+if __name__=="__main__":
+    main()
