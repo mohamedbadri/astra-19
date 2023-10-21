e948bcf98f6d23d8450bec09bbf8425208e6950f
mohmedbadri <56170819+mohamedbadri@users.noreply.github.com>
2023-10-20 17:26:21 -0700

    Add files via upload

---
+++ Session3.py
@@ -0,0 +1,16 @@
+def f(x):
+    return x**3 + 8
+
+# Main function
+def main():
+    x = 9
+    result = f(x)
+
+    print(result)
+
+    if result > 27:
+        print("YAY!")
+
+if __name__ == "__main__":
+    main()
+    
